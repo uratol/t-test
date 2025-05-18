db77218b020a3517526203338aa9640b74ffc876
Yurii Tolkunov <uratol@i-landlord.com>
2025-05-18 15:02:03 +0300

    Add assert_error_number to installation script. Add test execution to installation script.

--- install.sql
+++ install.sql
@@ -104,6 +104,35 @@ begin
 	return null
 end
 GO
+PRINT N'Creating Function [test].[assert_error_number]...';
+GO
+create function [test].[assert_error_number]
+(
+    @message nvarchar(max) -- Human-friendly description
+  , @expected_number int   -- Number you expect ERROR_NUMBER() to return
+)
+returns int
+as
+begin
+    declare @actual_number int = error_number(); -- inside CATCH
+
+    if @actual_number is null
+	       or @actual_number is distinct from @expected_number
+        return test.throw_error(concat(
+                                          @message
+                                        , '. '
+                                        , 'Error number '
+                                        , @expected_number
+                                        , ' expected. '
+                                        , 'Got '
+                                        , isnull(cast(@actual_number as nvarchar), '<null>')
+                                      )
+                               );
+
+    return null; -- assertion passed
+end
+GO
+
 PRINT N'Creating Function [test].[error]...';
 
 
@@ -742,4 +771,10 @@ end catch
 rollback
 GO
 PRINT N'Installation complete.';
+GO
+PRINT N'Run tests';
+GO
+exec test.run
+GO
+PRINT N'Installation and tests complete.';
 GO
